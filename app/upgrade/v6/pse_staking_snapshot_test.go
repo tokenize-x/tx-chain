b66f18a4c15504d73bd1dd06283d5b6cbbff8613
milad <mza1989@gmail.com>
2025-11-13 11:25:20 +0330

    create snapthot of staking module into pse after upgrade

---
+++ app/upgrade/v6/pse_staking_snapshot_test.go
@@ -0,0 +1,26 @@
+package v6_test
+
+import (
+	"testing"
+
+	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
+
+	"github.com/stretchr/testify/require"
+	v6 "github.com/tokenize-x/tx-chain/v6/app/upgrade/v6"
+	"github.com/tokenize-x/tx-chain/v6/testutil/simapp"
+)
+
+func TestSnapshotPSEStaking(t *testing.T) {
+	requireT := require.New(t)
+
+	testApp := simapp.New()
+	ctx := testApp.NewContext(false)
+
+	pseKeeper := testApp.PSEKeeper
+	stakingKeeper := testApp.StakingKeeper
+	addressCodec := testApp.AccountKeeper.AddressCodec()
+	valAddressCodec := testApp.StakingKeeper.ValidatorAddressCodec()
+	err := v6.SnapshotPSEStaking(ctx, stakingkeeper.NewQuerier(stakingKeeper), pseKeeper, addressCodec, valAddressCodec)
+	requireT.NoError(err)
+	// This is not an interesting test case, the real scenario can only be tested in integration tests.
+}
---
+++ integration-tests/upgrade/pse_staking_snapshot.go
@@ -0,0 +1,5 @@
+//go:build integrationtests
+
+package upgrade
+
+// TODO: we can add tests once queries are implemented.
