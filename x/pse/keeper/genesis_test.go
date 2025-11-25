243f99c237f0dc896a98bee9f1d6e6e5dbf44ca0
milad <mza1989@gmail.com>
2025-11-25 15:22:20 +0330

    finalize end blocker of pse module (#32)
    * finalize end blocker of pse module
    
    * Merge branch 'master' into milad/pse-endblock-rollback
    
    * pass linter
    
    * fixed a typo
    
    * addressed PR comments
    
    * Merge branch 'master' into milad/pse-endblock-rollback
    
    * Merge branch 'master' into milad/pse-endblock-rollback
    
    * Merge branch 'master' into milad/pse-endblock-rollback

--- x/pse/keeper/genesis_test.go
+++ x/pse/keeper/genesis_test.go
@@ -58,6 +58,7 @@ func TestGenesis(t *testing.T) {
 			Score:   sdkmath.NewInt(5678),
 		},
 	}
+	genesisState.SkipDistributions = true
 
 	err := pseKeeper.InitGenesis(ctx, genesisState)
 	requireT.NoError(err)
@@ -74,6 +75,7 @@ func TestGenesis(t *testing.T) {
 
 	requireT.EqualExportedValues(&genesisState.Params, &got.Params)
 	requireT.EqualExportedValues(&genesisState.ScheduledDistributions, &got.ScheduledDistributions)
+	requireT.Equal(genesisState.SkipDistributions, got.SkipDistributions)
 }
 
 // TestGenesis_EmptyState tests that default genesis state is valid and can be imported/exported.
