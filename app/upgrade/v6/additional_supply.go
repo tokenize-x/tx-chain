9f36ce66c6dcf27fac55def9a8091c2d23b3ec73
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2026-02-05 16:08:12 +0800

    fix linter

--- app/upgrade/v6/additional_supply.go
+++ app/upgrade/v6/additional_supply.go
@@ -18,6 +18,7 @@ const (
 	txSupplyForBinance     = 114_949_808_000_000
 )
 
+// TODO: replace with actual addresses.
 func mintAdditionalSupply(
 	ctx context.Context,
 	bankKeeper wbankkeeper.BaseKeeperWrapper,
@@ -31,9 +32,9 @@ func mintAdditionalSupply(
 
 	// SOLO HOLDERS
 	soloHoldersAddress := map[string]string{
-		string(constant.ChainIDMain): "core17pmq7hp4upvmmveqexzuhzu64v36re3w3447n7dt46uwp594wtps97qlm5", // TODO: replace with actual address
+		string(constant.ChainIDMain): "core17pmq7hp4upvmmveqexzuhzu64v36re3w3447n7dt46uwp594wtps97qlm5",
 		string(constant.ChainIDTest): "testcore1c6y9nwvu9jxx468qx3js620zq34c6hnpg9qgqu8rz3krjrxqmk9s5vzxkj",
-		string(constant.ChainIDDev):  "devcore17we2jgjyxexcz8rg29dn622axt7s9l263fl0zt", // TODO: replace with actual address
+		string(constant.ChainIDDev):  "devcore17we2jgjyxexcz8rg29dn622axt7s9l263fl0zt",
 	}[sdkCtx.ChainID()]
 	soloHoldersAcc, err := sdk.AccAddressFromBech32(soloHoldersAddress)
 	if err != nil {
@@ -50,9 +51,9 @@ func mintAdditionalSupply(
 
 	// BINANCE
 	binanceAddress := map[string]string{
-		string(constant.ChainIDMain): "core17pmq7hp4upvmmveqexzuhzu64v36re3w3447n7dt46uwp594wtps97qlm5", // TODO: replace with actual address
+		string(constant.ChainIDMain): "core17pmq7hp4upvmmveqexzuhzu64v36re3w3447n7dt46uwp594wtps97qlm5",
 		string(constant.ChainIDTest): "testcore19gcp0mkgml3l9pmm269000f6kxacpus0x5ru9pg95tt37dxjx0ksd30rx9",
-		string(constant.ChainIDDev):  "devcore17we2jgjyxexcz8rg29dn622axt7s9l263fl0zt", // TODO: replace with actual address
+		string(constant.ChainIDDev):  "devcore17we2jgjyxexcz8rg29dn622axt7s9l263fl0zt",
 	}[sdkCtx.ChainID()]
 	binanceAcc, err := sdk.AccAddressFromBech32(binanceAddress)
 	if err != nil {
