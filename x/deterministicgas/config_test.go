e33bd24350673ef2dd2b721f9c7a7a8cdb6c8931
TxCorpi0x <6095314+TxCorpi0x@users.noreply.github.com>
2026-03-11 19:37:04 +0300

    Move MsgSubmitProposal of group module to non-determinstic gas

--- x/deterministicgas/config_test.go
+++ x/deterministicgas/config_test.go
@@ -97,8 +97,8 @@ func TestDeterministicGas_DeterministicMessages(t *testing.T) {
 	// To make sure we do not increase/decrease deterministic and extension types accidentally,
 	// we assert length to be equal to exact number, so each change requires
 	// explicit adjustment of tests.
-	assert.Equal(t, 95, nondeterministicMsgCount)
-	assert.Equal(t, 68, deterministicMsgCount)
+	assert.Equal(t, 96, nondeterministicMsgCount)
+	assert.Equal(t, 67, deterministicMsgCount)
 	assert.Equal(t, 12, extensionMsgCount)
 	assert.Equal(t, 151, nonExtensionMsgCount)
 }
