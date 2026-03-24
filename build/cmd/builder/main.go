40945eec0593c8f37bbead21f0e4f2e529bf3214
TxCorpi0x <6095314+TxCorpi0x@users.noreply.github.com>
2026-03-24 14:15:48 +0300

    Register txd os image flag

--- build/cmd/builder/main.go
+++ build/cmd/builder/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	selfBuild "github.com/tokenize-x/tx-chain/build"
+	txchain "github.com/tokenize-x/tx-chain/build/tx-chain"
 	selfTools "github.com/tokenize-x/tx-chain/build/tools"
 	"github.com/tokenize-x/tx-crust/build"
 	"github.com/tokenize-x/tx-crust/build/tools"
@@ -12,5 +13,5 @@ func init() {
 }
 
 func main() {
-	build.Main(selfBuild.Commands)
+	build.Main(selfBuild.Commands, txchain.RegisterTXdOSFlag)
 }
